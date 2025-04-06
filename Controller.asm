b8a669775d21702c013979665fdd3b9434a19f99
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-04-06 15:12:47 +0200

    Update Controller.asm

--- Controller.asm
+++ Controller.asm
@@ -1,78 +1,173 @@
-section .data
-    menu db "Simple Controller:", 10
-         db "[o] Turn ON device", 10
-         db "[f] Turn OFF device", 10
-         db "[q] Quit", 10, 0
 
-    on_msg db "Device is now ON.", 10, 0
-    off_msg db "Device is now OFF.", 10, 0
-    quit_msg db "Exiting controller...", 10, 0
-    invalid_msg db "Invalid input. Try again.", 10, 0
+Syntax
 
-section .bss
-    input resb 1
+The syntax is similar as most assemblers are using. Every instruction must be on their own line. Labels are optional and must either start with a letter or a dot (.) and end with a colon.
 
-section .text
-    global _start
+label: instruction operands	; Comment
 
-_start:
-main_loop:
-    ; Print menu
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, menu
-    mov edx, 80
-    int 0x80
+Valid number formats for constants are:
 
-    ; Read user input
-    mov eax, 3          ; sys_read
-    mov ebx, 0          ; stdin
-    mov ecx, input
-    mov edx, 1
-    int 0x80
+Decimal: 200
+Decimal: 200d
+Hex: 0xA4
+Octal: 0o48
+Binary: 101b
 
-    ; Compare input
-    mov al, [input]
-    cmp al, 'o'
-    je turn_on
-    cmp al, 'f'
-    je turn_off
-    cmp al, 'q'
-    je quit
-    jmp invalid_input
+It is possible to define a number using a character or multiple numbers (see instruction DB) by using a string.
 
-turn_on:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, on_msg
-    mov edx, 20
-    int 0x80
-    jmp main_loop
+Character: 'A'
+String: "Hello World!"
 
-turn_off:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, off_msg
-    mov edx, 21
-    int 0x80
-    jmp main_loop
+Operands can either be one of the four general purpose registers, stack pointer register, a memory address or a constant. Stack pointer register can only be used as operand in MOV, ADD, SUB, CMP, INC and DEC instructions. Instead of defining an address as a constant or by using a register you can use labels. The assembler will then replace the label with the corresponding constant.
 
-invalid_input:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, invalid_msg
-    mov edx, 27
-    int 0x80
-    jmp main_loop
+General purpose (GP) register: A, B, C, D
+Stack pointer register: SP
+Address using a GP register: [A]
+Address using a GP register and offset: [D-3]
+Address using SP register and offset: [SP+2]
+Address using a constant: [100]
+Address using a label: label
+Constant: Any number between 0..255 (8bit unsigned)
+Offset for indirect addressing: Integer between -16..+15 (sign is mandatory)
 
-quit:
-    mov eax, 4
-    mov ebx, 1
-    mov ecx, quit_msg
-    mov edx, 26
-    int 0x80
+MOV - Copy a value
 
-    ; Exit program
-    mov eax, 1          ; sys_exit
-    xor ebx, ebx
-    int 0x80
+Copies a value from src to dest. The MOV instruction is the only one able to directly modify the memory. SP can be used as operand with MOV.
+
+MOV reg, reg
+MOV reg, address
+MOV reg, constant
+MOV address, reg
+MOV address, constant
+
+DB - Variable
+
+Defines a variable. A variable can either be a single number, character or a string.
+
+DB constant
+
+Math operations
+Addition and Subtraction
+
+Adds two numbers together or subtract one number form another. This operations will modify the carry and zero flag. SP can be used as operand with ADD and SUB.
+
+ADD reg, reg
+ADD reg, address
+ADD reg, constant
+SUB reg, reg
+SUB reg, address
+SUB reg, constant
+
+Increment and Decrement
+
+Increments or decrements a register by one. This operations will modify the carry and zero flag. SP can be used as operand with INC and DEC.
+
+INC reg
+DEC reg
+
+Multiplication and division
+
+Multiplies or divides the A register with the given value. This operations will modify the carry and zero flag.
+
+MUL reg
+MUL address
+MUL constant
+DIV reg
+DIV address
+DIV constant
+
+Logical instructions
+
+The following logical instructions are supported: AND, OR, XOR, NOT. This operations will modify the carry and zero flag.
+
+AND reg, reg
+AND reg, address
+AND reg, constant
+OR reg, reg
+OR reg, address
+OR reg, constant
+XOR reg, reg
+XOR reg, address
+XOR reg, constant
+NOT reg
+
+Shift instructions
+
+The following shift instructions are supported: SHL/SAL and SHR/SAR. As this simulator only supports unsigned numbers SHR and SAR yield the same result. This operations will modify the carry and zero flag.
+
+SHL reg, reg
+SHL reg, address
+SHL reg, constant
+SHR reg, reg
+SHR reg, address
+SHR reg, constant
+
+CMP - Compare
+
+Compares two values and sets the zero flag to true if they are equal. SP can be used as operand with CMP. Use this instruction before a conditional jump.
+
+CMP reg, reg
+CMP reg, address
+CMP reg, constant
+
+Jumps
+JMP - Unconditional jump
+
+Let the instruction pointer do a unconditional jump to the defined address.
+
+JMP address
+
+Conditional jumps
+
+Let the instruction pointer do a conditional jump to the defined address. See the table below for the available conditions.
+Instruction 	Description 	Condition 	Alternatives
+JC 	Jump if carry 	Carry = TRUE 	JB, JNAE
+JNC 	Jump if no carry 	Carry = FALSE 	JNB, JAE
+JZ 	Jump if zero 	Zero = TRUE 	JB, JE
+JNZ 	Jump if no zero 	Zero = FALSE 	JNE
+JA 	> 	Carry = FALSE && Zero = FALSE 	JNBE
+JNBE 	not <= 	Carry = FALSE && Zero = FALSE 	JA
+JAE 	>= 	Carry = FALSE 	JNC, JNB
+JNB 	not < 	Carry = FALSE 	JNC, JAE
+JB 	< 	Carry = TRUE 	JC, JNAE
+JNAE 	not >= 	Carry = TRUE 	JC, JB
+JBE 	<= 	C = TRUE or Z = TRUE 	JNA
+JNA 	not > 	C = TRUE or Z = TRUE 	JBE
+JE 	= 	Z = TRUE 	JZ
+JNE 	!= 	Z = FALSE 	JNZ
+CALL - Function call
+
+Call can be used to jump into a subroutine (function). Pushes the instruction address of the next instruction to the stack and jumps to the specified address.
+
+CALL address
+
+RET - Exit a subroutine
+
+Exits a subroutines by popping the return address previously pushed by the CALL instruction. Make sure the SP is balanced before calling RET otherwise the instruction pointer will have an ambiguous value.
+
+RET
+
+Stack instructions
+PUSH - Push to stack
+
+Pushes a value to the stack. The stack grows down and the current position is available in the stack pointer register (SP). This instruction will decrease the SP.
+
+PUSH reg
+PUSH address
+PUSH constant
+
+POP - Pop from stack
+
+Pops a value from the stack to a register. This instruction will increase the SP.
+
+POP reg
+
+Other instructions
+HLT - Stops the processor.
+
+Stops operation of the processor. Hit Reset button to reset IP before restarting.
+
+HLT
+
+
+https://schweigi.github.io/assembler-simulator/instruction-set.html
